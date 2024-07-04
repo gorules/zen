6c720fb16a57d4dee6b0cdcd782e72c0b4026886
Stefan <stefan@gorules.io>
2024-07-04 12:29:58 +0200

    feat: customizable fn timeout

--- core/engine/src/config.rs
+++ core/engine/src/config.rs
@@ -1,15 +1,19 @@
+use std::sync::atomic::{AtomicBool, AtomicU64};
+
 use once_cell::sync::Lazy;
-use std::sync::atomic::AtomicBool;
 
 #[derive(Debug)]
 pub struct ZenConfig {
     pub nodes_in_context: AtomicBool,
+    /// Function timeout presented in millis
+    pub function_timeout: AtomicU64,
 }
 
 impl Default for ZenConfig {
     fn default() -> Self {
         Self {
             nodes_in_context: AtomicBool::new(true),
+            function_timeout: AtomicU64::new(500),
         }
     }
 }

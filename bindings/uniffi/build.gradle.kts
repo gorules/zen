61dd80e6809d4f411462bcc34d10df98d1050185
Lars Werkman <lars@bitvavo.com>
2025-07-11 11:43:21 +0200

    feat: separate artifact for java and kotlin source code

--- bindings/uniffi/build.gradle.kts
+++ bindings/uniffi/build.gradle.kts
@@ -6,31 +6,82 @@ plugins {
 group = "io.gorules"
 version = "0.39.0"
 
-dependencies {
-    implementation("net.java.dev.jna:jna:5.15.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0")
-}
-
 repositories {
     mavenCentral()
 }
 
 sourceSets {
-    main {
-        kotlin {
-            srcDirs("build/generated/main/kotlin")
+    val java by creating {
+        java {
+            srcDirs("build/generated/java")
         }
         resources {
-            srcDirs("build/generated/main/resources")
+            srcDirs("build/generated/resources")
         }
+
+        compileClasspath += sourceSets["main"].compileClasspath
+        runtimeClasspath += sourceSets["main"].runtimeClasspath
+    }
+
+    val kotlin by creating {
+        kotlin {
+            srcDirs("build/generated/kotlin")
+        }
+        resources {
+            srcDirs("build/generated/resources")
+        }
+
+        compileClasspath += sourceSets["main"].compileClasspath
+        runtimeClasspath += sourceSets["main"].runtimeClasspath
+    }
+}
+
+dependencies {
+    implementation("net.java.dev.jna:jna:5.15.0")
+    "kotlinImplementation"("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0")
+}
+
+
+tasks {
+    val generateJavaJar by creating(Jar::class) {
+        archiveBaseName.set("zen_engine")
+        from(sourceSets["java"].output)
+        dependsOn(sourceSets["java"].classesTaskName)
+    }
+
+    val generateJavaSourcesJar by creating(Jar::class) {
+        archiveBaseName.set("zen_engine")
+        archiveClassifier.set("sources")
+        from(sourceSets["java"].allJava)
+    }
+
+    val generateKotlinJar by creating(Jar::class) {
+        archiveBaseName.set("zen_engine_kotlin")
+        from(sourceSets["kotlin"].output)
+        dependsOn(sourceSets["kotlin"].classesTaskName)
+    }
+
+    val generateKotlinSourcesJar by creating(Jar::class) {
+        archiveBaseName.set("zen_engine_kotlin")
+        archiveClassifier.set("sources")
+        from(sourceSets["kotlin"].kotlin)
     }
 }
 
 publishing {
     publications {
         create<MavenPublication>("mavenJava") {
-            from(components["java"])
-            artifact(tasks["kotlinSourcesJar"])
+            groupId = "io.gorules"
+            artifactId = "zen-engine"
+            artifact(tasks["generateJavaJar"])
+            artifact(tasks["generateJavaSourcesJar"])
+        }
+
+        create<MavenPublication>("mavenKotlin") {
+            groupId = "io.gorules"
+            artifactId = "zen-engine-kotlin"
+            artifact(tasks["generateKotlinJar"])
+            artifact(tasks["generateKotlinSourcesJar"])
         }
     }
     repositories {

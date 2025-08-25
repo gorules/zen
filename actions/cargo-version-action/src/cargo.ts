1e4135f5fb665d21237054501ac6d671e1b55e64
stefan-gorules <127550877+stefan-gorules@users.noreply.github.com>
2025-08-25 16:52:34 +0200

    fix: rust version bump (#387)
    * fix: rust version bump
    
    * add publish cmd

--- actions/cargo-version-action/src/cargo.ts
+++ actions/cargo-version-action/src/cargo.ts
@@ -7,11 +7,15 @@ type UpdateCargoOptions = {
 const versionRegex = /version = "[0-9]+\.[0-9]+\.[0-9]+"$/im;
 const expressionDep = /zen-expression =.*$/im;
 const templateDep = /zen-tmpl =.*$/im;
+const macroDep = /zen-macros =.*$/im;
+const typesDep = /zen-types =.*$/im;
 
 export const updateCargoContents = (contents: string, { version }: UpdateCargoOptions): string => {
   return contents
     .replace(versionRegex, `version = "${version}"`)
     .replace(expressionDep, `zen-expression = { path = "../expression", version = "${version}" }`)
+    .replace(macroDep, `zen-macros = { path = "../macros", version = "${version}" }`)
+    .replace(typesDep, `zen-types = { path = "../types", version = "${version}" }`)
     .replace(templateDep, `zen-tmpl = { path = "../template", version = "${version}" }`);
 };
 

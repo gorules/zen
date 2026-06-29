6c7bc2703922fe3d7d0ad7456bb0d154d1f728f8
stefan-gorules <127550877+stefan-gorules@users.noreply.github.com>
2026-06-29 13:54:19 +0200

    fix: uniffi add additional config fields (#473)

--- bindings/uniffi/src/config.rs
+++ bindings/uniffi/src/config.rs
@@ -4,11 +4,23 @@ use zen_engine::ZEN_CONFIG;
 #[derive(uniffi::Record)]
 pub struct ZenConfig {
     pub nodes_in_context: Option<bool>,
+    pub function_timeout_millis: Option<u32>,
+    pub http_auth: Option<bool>,
 }
 
 #[uniffi::export]
 pub fn override_config(config: ZenConfig) {
     if let Some(val) = config.nodes_in_context {
-        ZEN_CONFIG.nodes_in_context.store(val, Ordering::Relaxed)
+        ZEN_CONFIG.nodes_in_context.store(val, Ordering::Relaxed);
+    }
+
+    if let Some(val) = config.function_timeout_millis {
+        ZEN_CONFIG
+            .function_timeout_millis
+            .store(val as u64, Ordering::Relaxed);
+    }
+
+    if let Some(val) = config.http_auth {
+        ZEN_CONFIG.http_auth.store(val, Ordering::Relaxed);
     }
 }
